3857ffbb19a5d1d0b16e0dc77c402f9fd71dc4ac
Nick Koziupa <nickkoziupa@Nicks-MacBook-Pro.local>
2020-07-11 07:31:25 -0700

    Make loading button swap when load is complete

--- scripts/loadingcomplete.js
+++ scripts/loadingcomplete.js
@@ -1,2 +1,3 @@
 checkCookie();
-$("#splash-button").css("visibility", "visible");
+$("#splash-button").prop("disabled", false).html(
+  "<span class='spinner-border spinner-border-sm' role='status' aria-hidden='false'></span> Let's Go!";

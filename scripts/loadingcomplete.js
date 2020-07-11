6861904118cf67fadb1058f27ceadaa319013af6
Nick Koziupa <nickkoziupa@Nicks-MacBook-Pro.local>
2020-07-11 07:34:19 -0700

    Remove spinner from Let's Go button

--- scripts/loadingcomplete.js
+++ scripts/loadingcomplete.js
@@ -1,3 +1,3 @@
 checkCookie();
 $("#splash-button").prop("disabled", false).html(
-  "<span class='spinner-border spinner-border-sm' role='status' aria-hidden='false'></span> Let's Go!");
+  "<span role='status' aria-hidden='false'></span> Let's Go!");

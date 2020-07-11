9447709592e5b2c053fdf7c6d6c387a7b0740117
Nick Koziupa <nickkoziupa@Nicks-MacBook-Pro.local>
2020-07-11 07:32:24 -0700

    Fix loading button

--- scripts/loadingcomplete.js
+++ scripts/loadingcomplete.js
@@ -1,3 +1,3 @@
 checkCookie();
 $("#splash-button").prop("disabled", false).html(
-  "<span class='spinner-border spinner-border-sm' role='status' aria-hidden='false'></span> Let's Go!";
+  "<span class='spinner-border spinner-border-sm' role='status' aria-hidden='false'></span> Let's Go!");

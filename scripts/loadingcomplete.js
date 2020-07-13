f6624cb2089ccd23a254e9369ec9fb6b520e82ff
Nick Koziupa <nickkoziupa@Nicks-MacBook-Pro.local>
2020-07-12 20:31:24 -0700

    Finish editing loading icon

--- scripts/loadingcomplete.js
+++ scripts/loadingcomplete.js
@@ -1,4 +1,8 @@
+/* check cookie status */
+
 checkCookie();
+
+/* replace loading button with lets go button */
 
 $("#splash-button").prop("disabled", false).html(
   "<span role='status' aria-hidden='false'></span>Let's Go!");

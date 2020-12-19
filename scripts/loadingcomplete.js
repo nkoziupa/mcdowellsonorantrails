d83be51e8395d73004ae10303361c343d475cd76
Nick <mcdowellsonorantrails@gmail.com>
2020-12-19 14:09:50 -0700

    add initial url checker code

--- scripts/loadingcomplete.js
+++ scripts/loadingcomplete.js
@@ -2,6 +2,23 @@
 
 checkCookie();
 
+/* check url for shared route and create route if available */
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+console.log(urlParams.getAll('m')) // returns array of all markers;
+
+console.log(m);
+
+
+
+
+
+
+
+
+
+
 /* replace loading button with lets go button */
 
 $("#splash-button").prop("disabled", false).html(

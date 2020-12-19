031bd4f05b2e4ae1fae332591c715fff0acfc64d
Nick <mcdowellsonorantrails@gmail.com>
2020-12-19 14:53:51 -0700

    start adding marker array code

--- scripts/loadingcomplete.js
+++ scripts/loadingcomplete.js
@@ -6,16 +6,25 @@ checkCookie();
 
 var m = [];
 var sharedMarkers = [];
+var sharedMarker = "";
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 sharedMarkers = urlParams.getAll('m');
-
-// console.log(urlParams.getAll('m'));
-
 console.log(sharedMarkers);
 
+markerArray = sharedMarkers;
 
+// make all of the markers up to the last marker 'selected'
 
+for (var i = 0; i < markerArray.length - 1; i++) {
+  sharedMarker = "marker" + markerArray[i];
+  window[sharedMarker].setStyle({status: "selected", color: markerColorSelected, fillColor: markerFillColorSelected, fillOpacity: markerFillOpacitySelected});
+}
+
+// make the last marker the 'current' marker
+
+sharedMarker = "marker" + markerArray[markerArray.length];
+window[sharedMarker].setStyle({status: "current", color: markerColorCurrent, fillColor: markerFillColorCurrent, fillOpacity: markerFillOpacityCurrent});
 
 
 

824da015eba70ab04921d1a5ffb947c4cf00907b
Nick <mcdowellsonorantrails@gmail.com>
2020-12-20 16:56:20 -0700

    try the fire method

--- scripts/loadingcomplete.js
+++ scripts/loadingcomplete.js
@@ -2,6 +2,29 @@
 
 checkCookie();
 
+
+
+
+// TEST
+
+markerUR4.fire('click');
+markerCN5.fire('click');
+markerCN3.fire('click');
+markerUR5.fire('click');
+markerUR7.fire('click');
+markerBT6.fire('click');
+markerUR8.fire('click');
+markerCL1.fire('click');
+markerBR6.fire('click');
+
+
+
+
+
+
+
+
+
 /* replace loading button with lets go button */
 
 $("#splash-button").prop("disabled", false).html(

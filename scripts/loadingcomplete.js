23b1ba015587111283a7711ff431c3349017e02b
Nick <mcdowellsonorantrails@gmail.com>
2020-12-20 18:11:30 -0700

    switch to URLSearchParams

--- scripts/loadingcomplete.js
+++ scripts/loadingcomplete.js
@@ -5,8 +5,22 @@ checkCookie();
 
 
 
-// TEST
+// check url for shared route and create route if available
 
+var m = [];
+var sharedMarkers = [];
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+sharedMarkers = urlParams.getAll('m');
+// console.log(sharedMarkers);
+
+for (var i = 0; i < sharedMarkers.length; i++) {
+  fireMarker = "marker" + sharedMarkers[i];
+  window[fireMarker].fire('click');
+}
+
+/*
 markerUR4.fire('click');
 markerCN5.fire('click');
 markerCN3.fire('click');
@@ -16,6 +30,7 @@ markerBT6.fire('click');
 markerUR8.fire('click');
 markerCL1.fire('click');
 markerBR6.fire('click');
+*/
 
 // zoom in to show the route
 var sharedMarker;
@@ -27,12 +42,10 @@ for (var i = 0; i < markerArray.length; i++) {
   window[sharedMarker].addTo(sharedGroup);
 }
 
-/*
 for (var i = 0; i < segmentArray.length; i++) {
   sharedSegment = segmentArray[i] + "polyline";
   window[sharedSegment].addTo(sharedGroup);
 }
-*/
 
 map.fitBounds(sharedGroup.getBounds());
 

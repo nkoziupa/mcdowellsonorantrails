dac0fc11c31b4cfb24d5d8338a7daf4ad25a013e
Nick <mcdowellsonorantrails@gmail.com>
2021-01-05 07:18:01 -0700

    try vanilla js version of suggested route button text swap

--- scripts/suggestedroutes.js
+++ scripts/suggestedroutes.js
@@ -258,7 +258,9 @@ function viewSuggestedRoute(suggestedroute) {
 
   // change selected button text to spinner and loading text
 
-  $(suggestedRouteButton).html("<span class='spinner-border spinner-border-sm-suggestedroute' role='status' aria-hidden='true'></span> Loading...");
+  // $(suggestedRouteButton).html("<span class='spinner-border spinner-border-sm-suggestedroute' role='status' aria-hidden='true'></span> Loading...");
+
+  document.getElementById("suggestedRouteButton").innerHTML = "<span class='spinner-border spinner-border-sm-suggestedroute' role='status' aria-hidden='true'></span> Loading...";
 
   // don't show the tutorials
 

8e5e015a92ee1e105d2b28c0931ccd06550b46c2
Nick <mcdowellsonorantrails@gmail.com>
2022-02-23 20:35:52 -0700

    reposition pd2x

--- scripts/index.js
+++ scripts/index.js
@@ -659,12 +659,11 @@ var markerOP5 = L.circle([33.797710, -111.794445], new CreateMarker("OP5", "OP5"
 var markerOP11 = L.circle([33.806065, -111.806314], new CreateMarker("OP11", "OP11", 2816, ["DV13", "HW4", "OP9"])).addTo(map).on("click", onMapClick);
 var markerPD1X = L.circle([33.746265, -111.887555], new CreateMarker("PD1X", "Access Point", 2330, ["PD2X"])).addTo(map).on("click", onMapClick);
 var markerPD2X = L.circle([33.746551, -111.887373], new CreateMarker("PD2X", "Intersection", 2331, ["PD1X", "PD3X", "PDTX"])).addTo(map).on("click", onMapClick);
-var markerPD3X = L.circle([33.746649, -111.887495], new CreateMarker("PD3X", "Intersection", 2330, ["PDHX", "PD4X", "PD2X"])).addTo(map).on("click", onMapClick);
 
-markerPD3X.on({
+markerPD2X.on({
           mousedown: function () {
             map.on('mousemove', function (e) {
-              markerPD3X.setLatLng(e.latlng);
+              markerPD2X.setLatLng(e.latlng);
             });
           }
        });
@@ -673,6 +672,7 @@ markerPD3X.on({
          alert(e.latlng);
        })
 
+var markerPD3X = L.circle([33.746685, -111.887609], new CreateMarker("PD3X", "Intersection", 2330, ["PDHX", "PD4X", "PD2X"])).addTo(map).on("click", onMapClick);
 var markerPD4X = L.circle([33.747220, -111.887308], new CreateMarker("PD4X", "Intersection", 2337, ["PD3X", "RH1X", "AG2X"])).addTo(map).on("click", onMapClick);
 var markerPD5X = L.circle([33.746698, -111.886284], new CreateMarker("PD5X", "Intersection", 2340, ["PDTX", "HW2X", "PR5X", "HV75", "PD7X"])).addTo(map).on("click", onMapClick);
 var markerPD7X = L.circle([33.746302, -111.886432], new CreateMarker("PD7X", "Intersection", 2334, ["PDTX", "PD5X", "PD8X"])).addTo(map).on("click", onMapClick);
